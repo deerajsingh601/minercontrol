564cca55c43c37f5923dcf82b50983983a0b1f7f
Aman Singh <besoeasy@users.noreply.github.com>
2018-02-03 13:33:48 +0530

    Update go.cmd

--- go.cmd
+++ go.cmd
@@ -1,3 +1,3 @@
 git pull
 timeout 5
-EthDcrMiner64.exe -wd 1 -r 1 -epool eth-asia1.nanopool.org:9999 -ewal 0x0e00d8bc271a6121cbde6d542abc7185c0f9d983/besoeasy@yahoo.com -epsw x -mode 1
+EthDcrMiner64.exe
